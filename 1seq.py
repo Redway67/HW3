551e02f4c95f6fa73bf8038d1cf67d6ced103a2a
Igor <redway@mail.ru>
2019-10-15 00:04:34 +0600

    3

--- 1seq.py
+++ 1seq.py
@@ -1,1 +1,3 @@
-input('Введите количество элементов:')
+quantity_of_elements = int(input('Введите количество элементов:'))
+digit_list = [int(input(f'Введите число для {i} элемента списка:')) for i in range(quantity_of_elements)]
+print('Вывод: ', digit_list)
