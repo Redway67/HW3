d4e0f40ecf6007998e11853c7b1cc8f77bd8aeea
Igor <redway@mail.ru>
2019-10-15 20:02:19 +0600

    3

--- 2seq.py
+++ 2seq.py
@@ -1,6 +1,9 @@
 # МОДУЛЬ 2
 
-digits_str = input('Ввведите любые цифры через запятую:').split(',')
+# Если не использовать import re
+digits_str = input('Ввведите любые цифры через запятую (слэш или точку с запятой):') \
+    .replace(';', ',').replace('/', ',').split(',')
+
 digits = [int(i) for i in digits_str]
 
 result = [j for j in digits if digits.count(j) == 1]
