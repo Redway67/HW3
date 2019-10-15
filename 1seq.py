bc3bce46aac445533ab1fc07118550c913e3978e
Igor <redway@mail.ru>
2019-10-15 17:39:13 +0600

    3

--- 1seq.py
+++ 1seq.py
@@ -1,3 +1,9 @@
+# МОДУЛЬ 1
+
 quantity_of_elements = int(input('Введите количество элементов:'))
-digit_list = [int(input(f'Введите число для {i} элемента списка:')) for i in range(quantity_of_elements)]
+
+# инициализируем список с вводом числа
+digit_list = [int(input(f'Введите число для элемента списка с индексом {i}:')) for i in range(quantity_of_elements)]
+
 print('Вывод: ', digit_list)
+print('Длина списка = ', len(digit_list))
