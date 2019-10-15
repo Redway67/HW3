0eb4cb93da2d0b91e01e67073de91db5dff6f219
Igor <redway@mail.ru>
2019-10-15 20:38:27 +0600

    3

---
+++ 3seq.py
@@ -0,0 +1,13 @@
+# МОДУЛЬ 3
+
+# Первый список
+digits_str = input('Введите элементы 1-го списка (через запятую):').split(',')
+first_list = [int(i) for i in digits_str]
+
+# Второй список
+digits_str = input('Введите элементы 2-го списка (через запятую):').split(',')
+second_list = [int(i) for i in digits_str]
+
+result = [j for j in first_list if j not in second_list]
+
+print(result)
